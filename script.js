f75b5f14aac31bfb270c64951aabd61783792e3b
Alkesh kumar <127075190+Alkeshkumar94@users.noreply.github.com>
2023-12-31 11:11:38 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,10 @@
 //your JS code here. If required.
+let first=parseInt(prompt("enter first number"));
+let fir =first * 2;
+
+
+
+let second=parseInt(prompt("enter second number"));
+
+let result=parseInt(fir + second);
+alert("The final result is: "+result);
